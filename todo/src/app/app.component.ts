9b64b9eb4d3b84f4a2b684399a0c4df67048e093
tmh9x <thomas.hermanu@gmail.com>
2024-01-18 16:43:01 +0100

    angular tutorial

---
+++ todo/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { CommonModule, NgFor } from '@angular/common';
+
+import { Component } from '@angular/core';
+import { Item } from './item';
+
+@Component({
+  standalone: true,
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  imports: [CommonModule, NgFor],
+})
+export class AppComponent {
+  title = 'todo';
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    { description: 'eat', done: true },
+    { description: 'sleep', done: false },
+    { description: 'play', done: false },
+    { description: 'laugh', done: false },
+  ];
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) =>
+      this.filter === 'done' ? item.done : !item.done
+    );
+  }
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false,
+    });
+  }
+
+  remove(item: Item) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
+}
